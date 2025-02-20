c4a421a181ab4342a49ba101bac0b71a4eea4822
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-20 12:56:41 +0200

    Update oximeter-routes.js

---
+++ routes/oximeter-routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import OximeterData from '../models/oximeter-model.js';
+import { protect } from '../middlewares/authMiddleware.js';
+import {
+  getOximeterData,
+  addOximeterData,
+} from '../controllers/oximeter-controller.js';
+
+const oximeterRoute = express.Router();
+
+oximeterRoute.route('/').get(protect, getOximeterData);
+oximeterRoute.route('/:patientId').get(protect, getOximeterData);
+oximeterRoute.route('/add-oximeter').post(addOximeterData);
+
+export default oximeterRoute;
