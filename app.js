26b31b88b49fdb4f746a63d69ebc7be34e3381c4
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-20 12:56:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import usersRoutes from './routes/users-routes.js';
 import errorHandler from './controllers/error-controller.js';
+import ecgRoute from './routes/ecg-routes.js';
+import oximeterRoute from './routes/oximeter-routes.js';
 
 const app = express();
 dotenv.config();
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/v1/user', usersRoutes);
+app.use('/api/v1/ecg', ecgRoute);
+app.use('/api/v1/oximeter', oximeterRoute);
 
 app.use(errorHandler);
 export default app;
