92b313d925dfda23341789fdc00f32ad4d96cc55
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-04-07 14:52:47 +0200

    Update users-routes.js

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -5,5 +5,8 @@ const usersRoutes = express.Router();
 
 usersRoutes.route('/auth/signup').post(signup);
 usersRoutes.route('/auth/login').post(login);
+usersRoutes.route('/auth/logout').post();
+usersRoutes.route('/auth/me').get().post();
+
 
 export default usersRoutes;
