38e5f18c9282819a3fe153890064f32de1a22ca9
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-04-09 17:41:37 +0200

    Update user-controller.js
    Add 🆕
    - get patients
    - get one patient
    - add patient

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,3 +7,42 @@ import { getOne, getAll, deleteOne } from '../utils/controllers-handler.js';
 export const getAllUsers = getAll(User);
 export const getOneUser = getOne(User);
 export const deleteUser = deleteOne(User);
+
+export const getPatients = catchAsync(async (req, res, next) => {
+  const user = req.user;
+  if (user.role !== 'doctor')
+    return next(new AppError('You are not a Doctor', 403));
+  const patients = user.doctorInfo.patients;
+  sendResponse(res, 200, patients);
+});
+
+export const getOnePatient = catchAsync(async (req, res, next) => {
+  const user = req.user;
+  const patientIds = user.doctorInfo.patients;
+  console.log(patientIds);
+  const findPatient = patientIds.find((elememt) => elememt == req.params.pid);
+  if (!findPatient)
+    return next(new AppError('There is No Patient with This ID', 404));
+  console.log(findPatient);
+  sendResponse(res, 200, findPatient);
+});
+
+export const addPatient = catchAsync(async (req, res, next) => {
+  const user = req.user;
+  const patient = await User.findById(req.params.pid);
+  // 1) Check Patient in Database or not.
+  if (!patient)
+    return next(new AppError('There is No Patient with this ID', 404));
+  // 2) Add Patient to the Doctor Patient Id's Arrays
+  let patientIds = user.doctorInfo.patients;
+  patientIds.push(req.params.pid);
+  console.log(patientIds);
+  const newDoct = await User.findByIdAndUpdate(user._id, {
+    'doctorInfo.patients': patientIds,
+  });
+  // 3) Add Doctor Id to the PatientInfo primary Doctor
+  const newPatint = await User.findByIdAndUpdate(patient._id, {
+    'patientInfo.primaryDoctor': user._id,
+  });
+  sendResponse(res, 200, newDoct);
+});
