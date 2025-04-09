ebac80183319ecd64e6e9f5b9727e8f985416beb
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-04-09 15:13:46 +0200

    Update users-routes.js

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import { signup, login } from '../controllers/auth-controller.js';
+import { protect } from '../middlewares/auth-middleware.js';
+import {
+  getAllUsers,
+  getOneUser,
+  deleteUser,
+} from '../controllers/user-controller.js';
 
 const usersRoutes = express.Router();
 
@@ -10,14 +16,9 @@ const usersRoutes = express.Router();
  */
 usersRoutes.route('/auth/signup').post(signup);
 usersRoutes.route('/auth/login').post(login);
-usersRoutes.route('/auth/logout').post();
-usersRoutes.route('/auth/me').get().post();
 
-
-usersRoutes.route('/').get(); // Get All Users
-usersRoutes.route('/:id').get().patch().delete(); // Get Specific User
-
-usersRoutes.route('/doctor/patients').get(); // Get All Patient for this Doctor
-usersRoutes.route('/doctor/patients/:id').get(); // Get Specific Patient for this Doctor
+usersRoutes.use(protect);
+usersRoutes.route('/').get(getAllUsers);
+usersRoutes.route('/:id').get(getOneUser).delete(deleteUser);
 
 export default usersRoutes;
