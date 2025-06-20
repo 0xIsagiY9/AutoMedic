41cea8e44b80f29ab8e199b14d34e61d7872b25a
0xIsagiY9 <muhammed9alii9@gmail.com>
2025-06-20 15:37:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ import usersRoutes from './routes/users-routes.js';
 import errorHandler from './controllers/error-controller.js';
 import ecgRoute from './routes/ecg-routes.js';
 import oximeterRoute from './routes/oximeter-routes.js';
+import cors from 'cors';
 
 const app = express();
+// Configure CORS
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // Your React app's URL
+    credentials: true, // If you're using cookies/sessions
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 dotenv.config();
 
 app.use(express.json());
